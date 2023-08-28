11f1325925e17db5513044a24a892e9cbbd102b6
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-08-28 13:09:58 +0500

    [feat: Add feature of fetching data using axios]

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Product = () => {
-  return <></>;
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products")
+      .then(response => {
+        setProduct(response.data);
+      })
+      .catch(error => {
+        console.log("Error occurs while fetching data");
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Product Dashboard</h1>
+      <ul>
+        {product.map(item => (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>Price: ${item.price}</p>
+            <img src={item.image} alt={item.title} style={{ maxWidth: "100px" }} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Product;
